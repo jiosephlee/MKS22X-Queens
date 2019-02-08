9e0701512b81c4c14ee66dd93efc44facf679a9f
John Dogit config --global user.name John <jlee01@stuy.edu>
2019-02-08 10:09:07 -0500

    added debuge function

--- QueenBoard.java
+++ QueenBoard.java
@@ -41,6 +41,21 @@ public class QueenBoard{
             for(int j = 0; j < board[0].length; j++){
                 if(board[i][j] == -1){
                     output += 'Q';
+                } else{
+                    output+= '_';
+                }
+            }
+            output += '\n';
+        }
+        return output;
+    }
+
+    public String debug(){
+        String output = "";
+        for(int i = 0;i < board.length; i++){
+            for(int j = 0; j < board[0].length; j++){
+                if(board[i][j] == -1){
+                    output += 'Q';
                 } else if (board[i][j] != 0){
                     output += board[i][j];
                 } else{
